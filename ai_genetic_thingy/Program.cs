f3f3b677a65fe24ff4a3a1ac535a69ee32172473
simpleman19 <chancert413@gmail.com>
2017-12-09 17:51:59 -0600

    Initial commit

---
+++ ai_genetic_thingy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ai_genetic_thingy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
