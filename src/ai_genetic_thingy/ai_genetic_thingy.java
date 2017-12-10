30be8fd4edd48079d5476c48daed1e7c0790a164
Chance Turner <chance.turner@eagles.oc.edu>
2017-12-09 18:22:33 -0600

    Initial class

---
+++ src/ai_genetic_thingy/ai_genetic_thingy.java
@@ -0,0 +1,38 @@
+package ai_genetic_thingy;
+
+public class ai_genetic_thingy {
+	
+	public class Board {
+		// Blank is *
+		// Black is B
+		// Red is R
+		
+		char[][] board = new char[6][7];
+		
+		public Board() {
+			for (int i = 0; i < 6;  i++) {
+				for (int j = 0; j < 7; j++) {
+					board[i][j] = '*';
+				}
+			}
+		}
+		
+		public boolean isWinner() {
+			
+			return false;
+		}
+		
+		public char getWinner() {
+			
+			return 'b';
+		}
+	}
+	
+	public void main(String [] args) {
+		
+		Board gameBoard = new Board();
+		
+		
+		
+	}
+}
